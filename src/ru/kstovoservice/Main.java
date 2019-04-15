bd76591c050556cd2ebdec5eb09d9bb618b3ded2
AValov76 <43465649+AValov76@users.noreply.github.com>
2019-04-15 12:22:39 +0300

    bfls

--- src/ru/kstovoservice/Main.java
+++ src/ru/kstovoservice/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 
 public class Main {
@@ -33,16 +34,20 @@ public class Main {
             socket = createSocket();// сюды мы попадем только когда появится клиент...
             if (socket != null) {
                 System.out.println("Accepted. " + socket.getInetAddress());
-                try (OutputStream out = socket.getOutputStream();
-                     InputStream in = socket.getInputStream();) {
-
+                try {OutputStream out = socket.getOutputStream();
+                     InputStream in = socket.getInputStream();
                     byte[] buf = new byte[32 * 1024];
-                    int readbytes = in.read(buf); // блокировка ожидания ответа от клиента
+                    int readbytes = in.read(buf); // блокировка. ожидание ответа от клиента
+                    System.out.println("Получено " + readbytes + " байт.");
                     String line = new String(buf, 0, readbytes);
                     System.out.printf("Client> %s", line);
-
+                    System.out.println("Выше напечатано то, что пришло на сервер от клиента");
                     out.write(line.getBytes());
                     out.flush();
+
+                }
+                catch (SocketException e){
+                    System.out.println(e);
 
                 }
             }
