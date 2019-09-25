e6bfeb93209aeeb2b0dcef5ba5ab0cdf6a983abf
AValov76 <43465649+AValov76@users.noreply.github.com>
2019-09-25 11:49:36 +0300

    test from IJ

--- src/ru/kstovoservice/Main.java
+++ src/ru/kstovoservice/Main.java
@@ -9,7 +9,7 @@ import java.net.SocketException;
 
 public class Main {
 
-    public static final int PORT = 13539; // прописан проброс порта на 82.208.70.88 на комп
+    public static final int PORT = 13539; // прописан проброс порта на 82.208.70.88 на чувик
 
 
     public static Socket createSocket() {
@@ -17,7 +17,7 @@ public class Main {
         ServerSocket serverSocket = null;
         Socket socket = null;
         try {
-            // на сервере создаем специальный метод сокета, который умеет слушать порт и активизируется (возвращает сокет) только когда есть запрос на входящее подключение
+            //тут, на сервере, создаем специальный серверный сокет, который умеет слушать порт и активизируется (возвращает сокет) только когда есть запрос на входящее подключение
             serverSocket = new ServerSocket(PORT); //https://www.youtube.com/watch?v=SAsJuKuKTjE
             System.out.println("Started, waiting for connection");
             socket = serverSocket.accept(); // возвращает socket , если появилось внешнее соединение. Здесь метод accept разблокируется и возвращает обычный socket
@@ -30,12 +30,14 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Socket socket = null;
-            socket = createSocket();// сюды мы попадем только когда появится клиент...
+            Socket socket;
+            // сюды мы попадем только когда появится клиент... а до этого висим в этой точке (но там, на заднем плане может уже крутиться с десяток процессов)
+            socket = createSocket();
             if (socket != null) {
                 System.out.println("Accepted. " + socket.getInetAddress());
-                try {OutputStream out = socket.getOutputStream();
-                     InputStream in = socket.getInputStream();
+                try {
+                    OutputStream out = socket.getOutputStream();
+                    InputStream in = socket.getInputStream();
                     byte[] buf = new byte[32 * 1024];
                     int readbytes = in.read(buf); // блокировка. ожидание ответа от клиента
                     System.out.println("Получено " + readbytes + " байт.");
@@ -45,8 +47,7 @@ public class Main {
                     out.write(line.getBytes());
                     out.flush();
 
-                }
-                catch (SocketException e){
+                } catch (SocketException e) {
                     System.out.println(e);
 
                 }
